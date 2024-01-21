33207e071c9b92b778bb6ea5be649eff0c6f1186
ArchiGuru <jonny6015@icloud.com>
2024-01-22 01:44:26 +0800

    update

---
+++ addhook.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+clear
+
+echo -n "请输入项目[仓库]名称: "
+read -r REPO_NAME
+
+if [ -z "${REPO_NAME}" ]; then
+    echo "错误：输入的项目名称不正确。"
+    exit 1
+fi
+
+echo -n "请输入分支名称 (默认: main): "
+read -r BRANCH_NAME
+BRANCH_NAME="${BRANCH_NAME:-main}"
+
+ROOT_PATH="/opt/scripts/webhookd"
+SRC_PATH="/opt/src"
+
+mkdir -p "${ROOT_PATH}/logs/" "${ROOT_PATH}/deploy/" "${SRC_PATH}"
+
+# 创建 ${REPO_NAME}.sh 文件
+HOOK_SCRIPT="${ROOT_PATH}/${REPO_NAME}.sh"
+touch "$HOOK_SCRIPT"
+cat >"$HOOK_SCRIPT" <<EOF
+#!/bin/bash
+REPO_NAME="${REPO_NAME}"
+DEBUG=true
+# log 文件位置
+LOG_FILE="${ROOT_PATH}/logs/${REPO_NAME}_\$(date '+%Y%m%d_%H%M%S').log"
+# 函数
+payloadExit() { echo "错误: \$@" 1>&2 ; exit 1; }
+isDebug() {
+  [ "\$debug" = "true" ] && echo -e "Debug:\n \$@"
+}
+# 验证
+payload=\$1
+[ -z "\$payload" ] && payloadExit "payload 请求体不存在， 请检查配置。"
+# Debug
+isDebug "已收到 payload:\n \$payload"
+############# 这里是真正要执行的脚本 ################
+# 删除原有日志文件
+cd "${ROOT_PATH}/logs" || return
+COUNT_LOGS=$(ls -l | grep -c "${REPO_NAME}*") && echo "${COUNT_LOGS}"
+if [ "${COUNT_LOGS}" > 0 ] ; then
+  echo "❌ 存在日志，先删除："
+  rm -rf "${REPO_NAME}*" || return
+fi
+
+# 调用部署脚本
+cd "${ROOT_PATH}/deploy" || return
+nohup "${ROOT_PATH}/deploy/deploy_${REPO_NAME}.sh" > "\${LOG_FILE}" 2>&1 &
+####################################################
+echo "✅ 已成功调用部署脚本，并后台运行部署；请稍后查看结果。"
+echo "#########    ${REPO_NAME} hook 已发送完毕!   ########"
+exit 0;
+EOF
+
+echo "==============================================="
+echo "========== 已生成 ${REPO_NAME}.sh 文件。========="
+echo "==============================================="
+
+# 创建 deploy_${REPO_NAME}.sh 文件
+DEPLOY_SCRIPT="${ROOT_PATH}/deploy/deploy_${REPO_NAME}.sh"
+touch "$DEPLOY_SCRIPT"
+cat >"$DEPLOY_SCRIPT" <<EOF
+#!/bin/bash
+REPO_NAME="${REPO_NAME}"
+dateTime="\$(date '+%Y%m%d_%H%M%S')"
+echo "进入项目目录："
+cd "$SRC_PATH/${REPO_NAME}" || return
+git pull origin "${BRANCH_NAME}":"${BRANCH_NAME}"
+git reset --hard "origin/${BRANCH_NAME}"
+git stash clear
+git clean -d -fx .
+if [ -f ".gitmodules" ]; then
+  git submodule update --init
+  git submodule foreach git checkout "${BRANCH_NAME}"
+  git submodule foreach git pull origin "${BRANCH_NAME}"
+fi
+echo "✅ 已成功拉取最新代码！"
+
+#***********  这里编写其余逻辑,如编译/部署等操作  **************
+echo "***********     开始执行编译/部署操作：    ***************"
+
+
+#********************  部署逻辑结束  ***************************
+echo "****************************************"
+echo "        已成功部署 ${REPO_NAME}"
+echo "****************************************"
+exit 0;
+
+EOF
+
+echo "✅ 已生成部署文件 deploy_${REPO_NAME}.sh"
+chmod +x "$HOOK_SCRIPT"
+chmod +x "$DEPLOY_SCRIPT"
+echo "✅ 已添加执行权限！"
+echo -e "#######################    完成！    ######################\n"
+echo "✅ 已生成 ${REPO_NAME} 的 hook 脚本:"
+ls -g "$HOOK_SCRIPT"
+echo "✅ 已生成 ${REPO_NAME} 的部署脚本:"
+ls -g "$DEPLOY_SCRIPT"
+echo -e "\n⚠️ 请编辑 \"$ROOT_PATH/deploy/deploy_${REPO_NAME}.sh\" 来启用自动化部署。"
+exit 0
--- navstack.sh
+++ navstack.sh
@@ -1,35 +1,63 @@
 #!/bin/bash
+
 REPO_NAME="navstack"
-DEBUG="true"
-# log 文件位置
-LOG_FILE="/opt/scripts/webhookd/logs/navstack_$(date '+%Y%m%d_%H%M%S').log"
-# 函数
+ROOT_PATH="/opt/scripts/webhookd"
+LOG_FILE="${ROOT_PATH}/logs/navstack_$(date '+%Y%m%d_%H%M%S').log"
+
 payloadExit() {
-	echo "错误: $@" 1>&2
-	exit 1
+    echo "错误: $*" 1>&2
+    exit 1
 }
-isDebug() {
-	[ "$debug" = "true" ] && echo -e "Debug:\n $@"
-}
+
 # 验证
 payload=$1
-[ -z "$payload" ] && payloadExit "payload 请求体不存在， 请检查配置。"
-# Debug
-isDebug "已收到 payload:\n $payload"
+[ -z "${payload}" ] && payloadExit "payload 请求体不存在，请检查配置。"
+
 ############# 这里是真正要执行的脚本 ################
 # 删除原有日志文件
-cd "/opt/scripts/webhookd/logs" || return
-COUNT_LOGS=1 && echo ""
-if [ "" ] >0; then
-	echo "存在日志，先删除："
-	rm -rf "navstack*" || return
-fi
+cd "${ROOT_PATH}/logs" || return
+rm -rf "${REPO_NAME}"* || return
+
 # 调用部署脚本
-cd "/opt/scripts/webhookd/deploy" || return
-nohup "/opt/scripts/webhookd/deploy/deploy_navstack.sh" >"${LOG_FILE}" 2>&1 &
+cd "${ROOT_PATH}/deploy" || return
+echo "进入项目目录："
+cd "/opt/src/${REPO_NAME}" || return
+git pull origin "main":"main"
+git reset --hard "origin/main"
+git stash clear
+git clean -d -fx .
 
-####################################################
+if [ -f ".gitmodules" ]; then
+    git submodule update --init
+    git submodule foreach git checkout "main"
+    git submodule foreach git pull origin "main"
+fi
 
-echo "已成功调用部署脚本，并后台运行部署；请稍后查看结果。"
-echo "#########    navstack hook 已发送完毕!   ########"
+echo "✅ 已成功拉取最新代码！"
+
+#***********  这里编写其余逻辑,如编译/部署等操作  **************
+echo "***********     开始执行编译/部署操作：    ***************"
+
+echo "编译文件..."
+# 使用 hugo 生成静态网站，并压缩文件
+rm -rf "/opt/src/${REPO_NAME}/public/"
+hugo --minify
+# 使用 rsync 同步 /opt/src/navstack/public/ 目录到 /www/nav.archiguru.io 目录
+echo "同步文件..."
+rsync -a "/opt/src/${REPO_NAME}/public/" "/www/nav.archiguru.io"
+# 修改 /www/nav.archiguru.io 目录的所有者为 www 用户和 www 组
+echo "修改权限..."
+chown -R www:www "/www/nav.archiguru.io"
+# 重启 nginx 服务
+systemctl restart nginx.service
+# 输出提示信息
+echo "脚本执行完毕，网站已更新。"
+
+#********************  部署逻辑结束  ***************************
+echo "****************************************"
+echo "        已成功部署 ${REPO_NAME}"
+echo "****************************************"
+
+#echo "✅ 已成功调用部署脚本，并后台运行部署；请稍后查看结果。"
+#echo "#########    navstack hook 已发送完毕!   ########"
 exit 0
